663afbfa78eb08fbd2e5917fae31293cb59b3a28
madeel5564 <106953130+madeel5564@users.noreply.github.com>
2023-09-28 09:20:41 +0500

    added my ori
    added

---
+++ chane_of_responsibility/ConsoleLogger.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chane.of.responsibility;
+
+/**
+ *
+ * @author fa20-bse-163
+ */
+public class ConsoleLogger extends AbstractLogger {
+
+   public ConsoleLogger(int level){
+      this.level = level;
+   }
+
+   @Override
+   protected void write(String message) {		
+      System.out.println("Standard Console::Logger: " + message);
+   }
+}
