a6215102710bd142bae90c685dfc38de3f864063
madeel5564 <106953130+madeel5564@users.noreply.github.com>
2023-10-12 09:04:24 +0500

    add
    add

---
+++ Tamplate_Pattern/src/tamplate_pattern/TemplatePatternDemo.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tamplate_pattern;
+
+/**
+ *
+ * @author fa20-bse-163
+ */
+public class TemplatePatternDemo {
+   public static void main(String[] args) {
+
+      Game game = new Cricket();
+      game.play();
+      System.out.println();
+      game = new Football();
+      game.play();		
+   }
+}
